3d5088c3a88d1c1b3e1e6e9f2385de2d0ec4bca4
hemanth-hs <heamnthhs3399@gmail.com>
2021-01-27 20:25:38 +0530

    completed stage2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,12 +10,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+ return (
+   <p>
+     {props.part} {props.excercise}
+   </p>
+ ) 
+}
+
 const Content = (props) => {
   return (
     <>
-      <p>{props.part1} {props.excercise1}</p>
-      <p>{props.part2} {props.excercise2}</p>
-      <p>{props.part3} {props.excercise3}</p>
+      <Part part={props.part1} excercise={props.excercise1}/>
+      <Part part={props.part2} excercise={props.excercise2}/>
+      <Part part={props.part3} excercise={props.excercise3}/>
     </>
   )
 }
