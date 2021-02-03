6f1bb9a28e39b65c577c431ffe939f2f4dc5710a
hemanth-hs <heamnthhs3399@gmail.com>
2021-02-03 08:55:42 +0530

    completed unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,6 +8,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good+bad+neutral
+  const average = all / 3
+  const positive = 100*good / all
   
   return (
     <div>
@@ -19,6 +22,10 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average} </p>
+      <p>positive {positive} %</p>
+      <p></p>
     </div>
   )
 }
